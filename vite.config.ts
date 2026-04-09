4ff9055f6f8393dc209c3f9b900cc1128f6feeee
Your Name <you@example.com>
2026-04-09 12:15:04 +0200

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/chapeaudent-aldente/",
+  base: "/mission-locale/",
   plugins: [vue(), tailwindcss(), vueDevTools()],
   resolve: {
     alias: {
